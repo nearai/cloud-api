d86c7a5341a6254ac20f79872594e464a2ea4cda
Pierre Le Guen <26087574+PierreLeGuen@users.noreply.github.com>
2025-10-20 16:28:53 -0700

    fix: don't return model ID when in fact it's model name.

--- crates/inference_providers/tests/integration_tests.rs
+++ crates/inference_providers/tests/integration_tests.rs
@@ -11,7 +11,7 @@ use inference_providers::{
 use std::time::Duration;
 use tokio::time::timeout;
 
-const VLLM_BASE_URL: &str = "http://REDACTED_IP2:8000";
+const VLLM_BASE_URL: &str = "http://REDACTED_IP:8002";
 const VLLM_API_KEY: &str = "REDACTED";
 const TEST_TIMEOUT_SECS: u64 = 30;
 

f171e6bd647a81dd2511a3bc38d70c28ec6749a6
Pierre Le Guen <26087574+PierreLeGuen@users.noreply.github.com>
2025-10-20 15:18:18 -0700

    chore: better errors for batch_upsert.

--- crates/api/src/routes/admin.rs
+++ crates/api/src/routes/admin.rs
@@ -111,7 +111,7 @@ pub async fn batch_upsert_models(
                 _ => (
                     StatusCode::INTERNAL_SERVER_ERROR,
                     ResponseJson(ErrorResponse::new(
-                        "Failed to upsert models".to_string(),
+                        format!("Failed to upsert models, error: {:?}", e),
                         "internal_server_error".to_string(),
                     )),
                 ),

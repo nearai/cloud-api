a50d09f8ed9f73bb940b80d0c0e86b0bd9597042
Robert Yan <mstr.raphael@gmail.com>
2025-11-20 11:32:57 +0800

    fix: migration file

--- crates/database/src/migrations/sql/V0025__make_refresh_token_user_agent_not_null.sql
+++ crates/database/src/migrations/sql/V0025__make_refresh_token_user_agent_not_null.sql
@@ -1,10 +1,9 @@
--- Make user_agent NOT NULL in sessions table
+-- Make user_agent NOT NULL in refresh_tokens table
 -- First, update any existing NULL values to an empty string as a default
-UPDATE sessions
+UPDATE refresh_tokens
 SET user_agent = ''
 WHERE user_agent IS NULL;
 
 -- Now add the NOT NULL constraint
-ALTER TABLE sessions
+ALTER TABLE refresh_tokens
 ALTER COLUMN user_agent SET NOT NULL;
-
